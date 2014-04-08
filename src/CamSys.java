eb8149e73595f45f87bc6e1d870fbd32b9412735
olry <fabiooesch@hotmail.com>
2014-04-08 17:51:41 +0200

    uuuu

--- src/CamSys.java
+++ src/CamSys.java
@@ -3,22 +3,22 @@ public class CamSys {
 	private Matrix V = Matrix.identity(4);
 
 	public void rotateAbs(double phi, double a1, double a2, double a3) {
-		Matrix Rinv = Matrix.rotation(phi, a1, a2, a3);
+		Matrix Rinv = Matrix.rotation(-phi, a1, a2, a3);
 		V = V.times(Rinv);
 	}
 
 	public void translateAbs(double a1, double a2, double a3) {
-		Matrix Tinv = Matrix.translation(a1, a2, a3);
+		Matrix Tinv = Matrix.translation(-a1, -a2, -a3);
 		V = V.times(Tinv);
 	}
 
 	public void rotateRel(double phi, double a1, double a2, double a3) {
-		Matrix Rinv = Matrix.rotation(phi, a1, a2, a3);
+		Matrix Rinv = Matrix.rotation(-phi, a1, a2, a3);
 		V = Rinv.times(V);
 	}
 
 	public void translateRel(double a1, double a2, double a3) {
-		Matrix Tinv = Matrix.translation(a1, a2, a3);
+		Matrix Tinv = Matrix.translation(-a1, -a2, -a3);
 		V = Tinv.times(V);
 	}
 
